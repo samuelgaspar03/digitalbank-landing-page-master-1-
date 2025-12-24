cb820322b85554d777161336f238e4a297faaa06
Samuel Gaspar <samuelgspr@gmail.com>
2025-12-24 05:07:31 -0300

    Implementação da animação do menu Hamburguer.

--- digitalbank-landing-page-master/app/js/script.js
+++ digitalbank-landing-page-master/app/js/script.js
@@ -1,1 +1,20 @@
-console.log('hello word!');
+const btnHamburguer = document.querySelector('#btnHamburguer');
+const header = document.querySelector('.header'); 
+const overlay = document.querySelector('.overlay');
+
+btnHamburguer.addEventListener('click', function(){
+    console.log('click hamburguer');
+
+    if(header.classList.contains('open')){ //close hamburguer menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+
+    }
+    else{
+        header.classList.add('open'); //open hamburguer menu
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+
+});
